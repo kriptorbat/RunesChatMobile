00be372dd2748b563de4b446c3fcc710808f8d78
kriptorbat <stasan.asanov@gmail.com>
2023-07-15 23:34:29 +0300

    sign in disign 1

---
+++ app/src/main/java/com/example/runeschat/activitys/sign_up.kt
@@ -0,0 +1,12 @@
+package com.example.runeschat.activitys
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.runeschat.R
+
+class sign_up : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
